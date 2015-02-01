5c0acee50f8cbd92a257b6f1ef41ad3ecb7fa6ae
Alexander Robinson <robinson@fis.ucm.es>
2015-02-01 10:13:59 +0100

    Improved ens_init to be highly generic. Only writes dimensions.

--- ens_rembo.f90
+++ ens_rembo.f90
@@ -13,30 +13,57 @@ program ens_rembo
     character(len=512) :: ens_fldr, filename 
     character(len=512), allocatable :: names(:), units(:)
 
-    double precision, allocatable :: time_in(:), time_out(:)
-    character(len=256) :: tunits
+    double precision, allocatable :: time_in(:), time_out(:), x(:), y(:)
+    character(len=256) :: tname, tunits, xname, xunits, yname, yunits
 
-    integer :: nsim, nt 
+    integer :: nsim, nt, nx, ny  
 
     integer :: k 
 
     ens_fldr = "output"
-    filename = "sico.nc"
 
     allocate(names(3),units(3)) 
     names = ["var1","var2","var3"]
     units = ["m","m","m"]
 
-    nt = 10 
-    allocate(time_out(nt))
-    do k = 1, nt 
-        time_out = dble(k)*2.d0
-    end do 
+    tname  = "time"
+    tunits = "ka BP"
+
+    xname  = "xc"
+    xunits = "km"
+
+    yname  = "yc"
+    yunits = "km" 
+
 
     nsim = 5 
 
-    call ens_init_1D(ens_fldr,filename,names,units,time_out,tunits="ka BP",nsim=nsim)
+    nt = 10 
+    allocate(time_out(nt))
+    do k = 1, nt 
+        time_out(k) = dble(k)*2.d0
+    end do 
 
+    nx = 12 
+    allocate(x(nx))
+    do k = 1, nx 
+        x(k) = dble(k)*2.d0
+    end do 
+
+    ny = 24 
+    allocate(y(ny))
+    do k = 1, ny 
+        y(k) = dble(k)*2.d0
+    end do
+
+    ! 1D ensemble file
+    filename = "sico.nc"
+    call ens_init(ens_fldr,filename,nsim,t=time_out,tname=tname,tunits=tunits)
+
+    ! 2D ensemble file 
+    filename = "sico2D.nc"
+    call ens_init(ens_fldr,filename,nsim,x=x,xname=xname,xunits=xunits, &
+                  t=time_out,tname=tname,tunits=tunits)
 
 
 
