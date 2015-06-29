2b7807b8642628dac91e9f08a978ded744888156
Alexander Robinson <robinson@fis.ucm.es>
2015-06-29 22:00:36 +0300

    mis11

--- ens_rembo-mis11.f90
+++ ens_rembo-mis11.f90
@@ -23,7 +23,7 @@ program ens_rembo
     ! 1. Define output folder and get ensemble
     !    input folders 
     ! =============================================
-    ens_fldr = "output/mis11x_m11"
+    ens_fldr = "output/mis11z_m11"
     call ens_folders(fldrs,path=ens_fldr)
 
     ! =============================================
