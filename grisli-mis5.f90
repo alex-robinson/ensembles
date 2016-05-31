26743575a7c9f46ac26df70d65919ec323ce9040
Alexander Robinson <robinson@fis.ucm.es>
2016-05-31 12:11:14 +0200

    Testing grisli ensembles.

--- grisli-mis5.f90
+++ grisli-mis5.f90
@@ -44,67 +44,42 @@ program ens_grisli
     call ens_write_par(ens_fldr,fldrs,filename="grisli_par_Greenland.nml",fmt="nml", &
                        names=["bmelt_par:kappa_grz ","bmelt_par:kappa_shlf"])
 
-    stop 
-
-    ! ## SICO 1D (time) ##
-    filename = "sico.1d.nc"
+    ! ## GRISLI 1D ##
+    filename = "Grisli15_1D_Global.nc"
     call ens_init(ens_fldr,fldrs,filename,names=["time"],t=time1D,tname=tname,tunits=tunits)
 
-    call ens_write(ens_fldr,fldrs,filename,"Vtot")
-    call ens_write(ens_fldr,fldrs,filename,"Aib")
-    call ens_write(ens_fldr,fldrs,filename,"dVdt")
-    call ens_write(ens_fldr,fldrs,filename,"zs_max")
-    call ens_write(ens_fldr,fldrs,filename,"z_sle")
-    call ens_write(ens_fldr,fldrs,filename,"z_sl")
-
-    call ens_write(ens_fldr,fldrs,filename,"V_south")
-    call ens_write(ens_fldr,fldrs,filename,"A_south")
-
+    call ens_write(ens_fldr,fldrs,filename,"isvol")
+    call ens_write(ens_fldr,fldrs,filename,"isvolf")
+    call ens_write(ens_fldr,fldrs,filename,"hmean_")
+    call ens_write(ens_fldr,fldrs,filename,"Hmax_")
+    call ens_write(ens_fldr,fldrs,filename,"idx_at")
+    call ens_write(ens_fldr,fldrs,filename,"idx_ap")
+    call ens_write(ens_fldr,fldrs,filename,"idx_ao")
+    call ens_write(ens_fldr,fldrs,filename,"idx_bt")
+    call ens_write(ens_fldr,fldrs,filename,"idx_bp")
+    call ens_write(ens_fldr,fldrs,filename,"idx_bo")
+    call ens_write(ens_fldr,fldrs,filename,"idx_sl")
+    
     ! ## SICO 2D (2D+time) ##
-    filename = "sico.nc"
-    call ens_init(ens_fldr,fldrs,filename,names=["x   ","y   ","time"],static=["lon","lat"], &
+    filename = "Grisli15_2D.nc"
+    call ens_init(ens_fldr,fldrs,filename,names=["xc  ","yc  ","time"],static=["lon","lat"], &
                   t=time2D,tname=tname,tunits=tunits)
 
     ! Write fields with integer precision (rounds to the meter) to save space
-    call ens_write(ens_fldr,fldrs,filename,"mask",method="align",prec="int")
-    call ens_write(ens_fldr,fldrs,filename,"zs",  method="align",prec="int")
-    call ens_write(ens_fldr,fldrs,filename,"zb",  method="align",prec="int")
-    call ens_write(ens_fldr,fldrs,filename,"H",   method="align",prec="int")
-    call ens_write(ens_fldr,fldrs,filename,"H_t", method="align",prec="int")
-
-    ! ## REMBO 2D (month+time) ##
-    filename = "rembo.gis.nc"
-    call ens_init(ens_fldr,fldrs,filename,names=["point","month","time "],t=time1D,tname=tname,tunits=tunits)
-
-    call ens_write(ens_fldr,fldrs,filename,"tt")
-    call ens_write(ens_fldr,fldrs,filename,"tte")
-    call ens_write(ens_fldr,fldrs,filename,"pp")
-    call ens_write(ens_fldr,fldrs,filename,"snow")
-    call ens_write(ens_fldr,fldrs,filename,"runoff")
-    call ens_write(ens_fldr,fldrs,filename,"smb")
-    call ens_write(ens_fldr,fldrs,filename,"dT")
-    call ens_write(ens_fldr,fldrs,filename,"dTb")
-    call ens_write(ens_fldr,fldrs,filename,"S")
-    ! call ens_write(ens_fldr,fldrs,filename,"S65")
-    call ens_write(ens_fldr,fldrs,filename,"aco2")
-
-    ! Forcing too 
-    call ens_write(ens_fldr,fldrs,filename,"dT_jja")
-!     call ens_write(ens_fldr,fldrs,filename,"dT_amp")
-
-    ! ## REMBO 3D (2D+time) ##
-    filename = "clima.nc"
-    call ens_init(ens_fldr,fldrs,filename,names=["x   ","y   ","time"],static=["lon","lat"], &
-                  t=time2D,tname=tname,tunits=tunits)
-
-    call ens_write(ens_fldr,fldrs,filename,"mask",method="align",prec="int")
-    call ens_write(ens_fldr,fldrs,filename,"zs",  method="align",prec="int")
-    call ens_write(ens_fldr,fldrs,filename,"tjja",method="align")
-    call ens_write(ens_fldr,fldrs,filename,"tann",method="align")
-    call ens_write(ens_fldr,fldrs,filename,"tjan",method="align")
-    call ens_write(ens_fldr,fldrs,filename,"tjul",method="align")
-    call ens_write(ens_fldr,fldrs,filename,"pp",  method="align")
-    call ens_write(ens_fldr,fldrs,filename,"pdds",method="align")
+    call ens_write(ens_fldr,fldrs,filename,"S",     method="align",prec="int")
+    call ens_write(ens_fldr,fldrs,filename,"H",     method="align",prec="int")
+    call ens_write(ens_fldr,fldrs,filename,"Bsoc",  method="align",prec="int")
+    call ens_write(ens_fldr,fldrs,filename,"smb",   method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"Bmelt", method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"dTshlf",method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"kappa", method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"Tjja",  method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"Tb0",   method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"Hwat",  method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"U",     method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"Ub",    method="align",prec="float")
+    call ens_write(ens_fldr,fldrs,filename,"posx",  method="align",prec="int")
+    call ens_write(ens_fldr,fldrs,filename,"posy",  method="align",prec="int")
 
     write(*,*)
     write(*,*) "Ensemble generation completed."
