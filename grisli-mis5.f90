d20deaf23b3725fa6c12258afbc7c34ed3ccc31b
Alexander Robinson <robinson@fis.ucm.es>
2018-01-29 20:50:30 +0100

    semi-marine processing.

--- grisli-mis5.f90
+++ grisli-mis5.f90
@@ -21,7 +21,7 @@ program ens_grisli
     !    input folders 
     ! =============================================
     !ens_fldr = "grisli-ucm/output/iso-ilaria/iso-500yr"
-    ens_fldr = "grisli-ucm/output/iso-ilaria-1/itm/"
+    ens_fldr = "grisli-ucm/output/iso-ilaria-1/pdd/"
     call ens_folders(fldrs,path=ens_fldr)
 
     ! =============================================
