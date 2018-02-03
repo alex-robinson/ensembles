7021e4b342a36efdbdc383d84929df8ac2d0ed9a
Alexander Robinson <robinson@fis.ucm.es>
2018-02-03 21:27:08 +0100

    Testing w semi-marine processing.

--- grisli-mis5.f90
+++ grisli-mis5.f90
@@ -22,8 +22,8 @@ program ens_grisli
     !    input folders 
     ! =============================================
 
-    ens_fldr     = "/home/itabone/grisli-ucm/output/v0.32/iso/new/pdd/"
-    ens_fldr_out = "grisli-ucm/output/iso-ilaria-1/pdd/"
+    ens_fldr     = "grisli-ucm-itabone/output/v0.32/iso/new/ens1/"
+    ens_fldr_out = "grisli-ucm/output/iso-ilaria-1/ens1/"
 
     call ens_folders(fldrs,path=ens_fldr,path_out=ens_fldr_out)
 
