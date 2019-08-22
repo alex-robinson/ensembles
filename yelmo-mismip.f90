7968550ec2f98e8a97eceb87174fec3ba5c0dd9b
Alexander Robinson <robinson@fis.ucm.es>
2019-08-22 15:51:53 +0300

    Updated ensembles for yelmo-mismip.

--- yelmo-mismip.f90
+++ yelmo-mismip.f90
@@ -53,8 +53,8 @@ program ens_yelmo
     ! =============================================
     tname  = "time"
     tunits = "years"
-    call ens_times(time1D,par=[  0.d3,150.d3,0.5d3] )
-    call ens_times(time2D,times=[0.d3,20.d3,150.d3] )
+    call ens_times(time1D,par=[  0.d3,85.d3,0.1d3] )
+    call ens_times(time2D,times=[0.d3,25.d3,85.d3] )
 
     ! =============================================
     ! 4. Write ensemble files 
